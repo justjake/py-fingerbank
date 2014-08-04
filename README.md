bec5d18d3d627cb3bc48d646181592414b59bf66
Jake Teton-Landis <just.1.jake@gmail.com>
2014-08-03 18:16:32 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# py-fingerbank
+
+A [fingerbank](fb) support library for Python 2.6 and above.
+
+(c) 2014 Jake Teton-Landis.
+
+LGPL terms.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='py-fingerbank',
+      version='0.1.0a1',
+      description='DHCP fingerprint lookups',
+      author='Jake Teton-Landis',
+      author_email='just.1.jake@gmail.com',
+      url='https://github.com/justjake/py-fingerbank',
+      packages=[
+          'fingerbank',
+          ],
+      install_requires=[
+        ])
